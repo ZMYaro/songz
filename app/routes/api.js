9ab9cf19015519d58751d0d47825a445893d4acf
Zachary Yaro <zmyaro@zmyaro.com>
2021-03-29 03:34:43 -0400

    Add the ability to query for a specific song

--- app/routes/api.js
+++ app/routes/api.js
@@ -22,9 +22,59 @@ router.all((req, res, next) => {
 });
 
 router.route('/songs')
-	/** Get the list of songs in the library. */
+	/**
+	 * Get the list of all songs in the library or all that match the parameters exactly.
+	 * [gdrive-flac] - The GDrive ID of the song's FLAC audio file
+	 * [gdrive-m4a] - The GDrive ID of the song's M4A audio file
+	 * [gdrive-mp3] - The GDrive ID of the song's MP3 audio file
+	 * [gdrive-ogg] - The GDrive ID of the song's Ogg audio file
+	 * [gdrive-art] - The GDrive ID of the song's album art file
+	 * [title] - The song's title
+	 * [duration] - The duration of the song, in milliseconds
+	 * [track-no] - The track number of the song on its disc
+	 * [disc-no] - The disc number of the song
+	 * [year] - The year the song was released
+	 * [genre] - The name of the song's genre
+	 * [artist] - The name(s) of the song's artist(s), semicolon-separated
+	 * [composer] - The name(s) of the song's composer(s), semicolon-separated
+	 * [album-title] - The title of the album the song belongs to
+	 * [album-artist] - The album artist of the album the song belongs to
+	 */
 	.get(async function (req, res) {
-		var songs = await populateSong(Song.find({}).sort({ title: 'asc', artist: 'asc', album: 'asc' }));
+		var params = {};
+		// Assume any param that was passed is intended to be included,
+		// but trim or parse them to the correct formats before querying.
+		if (req.query['gdrive-flac']) { params.gDriveFLAC = req.query['gdrive-flac'].trim(); }
+		if (req.query['gdrive-m4a']) {  params.gDriveM4A = req.query['gdrive-m4a'].trim(); }
+		if (req.query['gdrive-mp3']) {  params.gDriveMP3 = req.query['gdrive-mp3'].trim(); }
+		if (req.query['gdrive-ogg']) {  params.gDriveOgg = req.query['gdrive-ogg'].trim(); }
+		if (req.query['gdrive-art']) {  params.gDriveArt = req.query['gdrive-art'].trim(); }
+		if (req.query['title']) {       params.title = req.query['title'].trim(); }
+		if (req.query['duration']) {    params.duration = parseFloat(req.query['duration']); }
+		if (req.query['track-no']) {    params.trackNo = Math.floor(parseInt(req.query['track-no'])); }
+		if (req.query['disc-no']) {     params.discNo = Math.floor(parseInt(req.query['disc-no'])); }
+		if (req.query['year']) {        params.year = Math.floor(parseInt(req.query['year'])); }
+		
+		if (req.query['genre']) {
+			params.genre = await Genre.findOne({ name: req.query['genre'].trim() });
+		}
+		if (req.query['artist']) {
+			params.artist = { $all: await Artist.findFromStrList(req.query['artist']) };
+		}
+		if (req.query['composer']) {
+			params.composer = { $all: await Artist.findFromStrList(req.query['composer']) };
+		}
+		if (req.query['album-title']) {
+			var albumParams = {
+				title: req.query['album-title'].trim()
+			};
+			if (req.query['album-artist']) {
+				albumParams.artist = { $all: await Artist.findFromStrList(req.query['album-artist']) };
+			}
+			params.album = await Album.findOne(albumParams);
+		}
+		
+		var songs = await populateSong(Song.find(params).sort({ title: 'asc', artist: 'asc', album: 'asc' }));
 		res.json(songs);
 	})
 	/**
