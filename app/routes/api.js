3fbbb5dc6eb377e30918864fd959f57289790b7f
Zachary Yaro <zmyaro@zmyaro.com>
2021-04-03 00:51:54 -0400

    Add ability to request the number of playthroughs for a song instead of the full array

--- app/routes/api.js
+++ app/routes/api.js
@@ -264,13 +264,15 @@ router.route('/playthroughs/:songId')
 	 * Get the number of playthroughs for a song.
 	 * [before] - The timestamp to get playthroughs before (defaults to infinity)
 	 * [after] - The timestamp to get playthroughs after  (defaults to the epoch)
+	 * [count] - If included, return the number of playthroughs instead of the actual data
 	 */
 	.get(async function (req, res) {
 		// TODO: Implement before and after.
 		var params = {},
 			songId = req.params.songId,
 			beforeTimestamp = parseInt(req.query['before']),
-			afterTimestamp = parseInt(req.query['after']);
+			afterTimestamp = parseInt(req.query['after']),
+			returnCount = (typeof req.query['count'] !== 'undefined');
 		
 		if (!mongoose.Types.ObjectId.isValid(songId)) { return handleError(res, 'Song not found.', 404); }
 		
@@ -291,8 +293,15 @@ router.route('/playthroughs/:songId')
 			if (isNaN(afterTimestamp)) { return handleError(res, 'Invalid after timestamp.', 422); }
 			params.timestamp.$gt = new Date(afterTimestamp);
 		}
-		var playthroughs = await Playthrough.find(params);
-		res.json(playthroughs);
+		
+		if (returnCount) {
+			var playthroughCount = await Playthrough.countDocuments(params);
+			res.set('Content-Type', 'text/plain');
+			res.send('' + playthroughCount);
+		} else {
+			var playthroughs = await Playthrough.find(params);
+			res.json(playthroughs);
+		}
 	})
 	/**
 	 * Add a playthrough to a song.
