afaec62779eea919aa1301b37d30f8bf6c25a143
Zachary Yaro <zmyaro@zmyaro.com>
2021-12-16 02:56:11 -0500

    Improve edit song dialog styles
    Not final Material style, but at least adds the emoji labels the add song page has.

--- app/static/components/songz-edit-song-dialog.js
+++ app/static/components/songz-edit-song-dialog.js
@@ -101,70 +101,70 @@ export class SongZEditSongDialog extends LitElement {
 							<input type="hidden" name="id" .value="${this.song?._id}" />
 						</label>
 						<label>
-							GDrive FLAC:
+							📄 GDrive FLAC:
 							<input type="text" name="gdrive-flac" .value="${this.song?.gDriveFLAC || ''}" />
 						</label>
 						<label>
-							GDrive M4A:
+							📄 GDrive M4A:
 							<input type="text" name="gdrive-m4a" .value="${this.song?.gDriveM4A || ''}" />
 						</label>
 						<label>
-							GDrive MP3:
+							📄 GDrive MP3:
 							<input type="text" name="gdrive-mp3" .value="${this.song?.gDriveMP3 || ''}" />
 						</label>
 						<label>
-							GDrive Ogg:
+							📄 GDrive Ogg:
 							<input type="text" name="gdrive-ogg" .value="${this.song?.gDriveOgg || ''}" />
 						</label>
 						<label>
-							GDrive Art:
+							🖼 GDrive Art:
 							<input type="text" name="gdrive-art" .value="${this.song?.gDriveArt || ''}" />
 						</label>
 						<label>
-							GDrive LRC:
+							📃 GDrive LRC:
 							<input type="text" name="gdrive-lrc" .value="${this.song?.gDriveLRC || ''}" />
 						</label>
 						<label>
-							Title:
+							🆎 Title:
 							<input type="text" name="title" .value="${this.song?.title}" />
 						</label>
 						<label>
-							Duration:
-							<small>In milliseconds</small>
+							🕓 Duration:
+							<small>(in milliseconds)</small>
 							<input type="number" name="duration" step="0.1" .value="${this.song?.duration}" />
 						</label>
 						<label>
-							Genre:
+							🎹 Genre:
 							<input type="text" name="genre" .value="${this.song?.genre?.name || ''}" />
 						</label>
 						<label>
-							Artist(s):
-							<small>Semicolon-separated</small>
+							🧑‍🎤 Artist(s):
+							<small>(semicolon-separated)</small>
 							<input type="text" name="artist" .value="${formatArtist(this.song, true)}" />
 						</label>
 						<label>
-							Composer(s):
-							<small>Semicolon-separated</small>
+							👤 Composer(s):
+							<small>(semicolon-separated)</small>
 							<input type="text" name="composer" .value="${formatArtist({artist: this.song?.composer}, true)}" />
 						</label>
 						<label>
-							Album title:
+							💿 Album title:
 							<input type="text" name="album-title" .value="${formatAlbum(this.song, true)}" />
 						</label>
 						<label>
-							Album artist:
+							🧑‍🎤 Album artist:
 							<input type="text" name="album-artist" .value="${formatArtist({artist: this.song?.album?.artist}, true)}" />
 						</label>
 						<label>
-							Track #:
+							🔢 Track #:
 							<input type="number" name="track-no" .value="${this.song?.trackNo || ''}" />
 						</label>
 						<label>
-							Disc #:
+							🔢 Disc #:
 							<input type="number" name="disc-no" .value="${this.song?.discNo || ''}" />
 						</label>
 						<label>
-							Year:
+							📅 Year:
 							<input type="number" name="year" min="1000" max="9999" .value="${this.song?.year || ''}" />
 						</label>
 					</fieldset>
