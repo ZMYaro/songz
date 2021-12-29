683fc80035118148c2cd0e70e31ee95e886b00fa
Zachary Yaro <zmyaro@zmyaro.com>
2021-12-28 22:40:58 -0500

    Move Google OAuth scopes to const

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -4,9 +4,14 @@ const express = require('express'),
 	passport = require('passport'),
 	path = require('path');
 
+const GOOGLE_SCOPES = [
+	'https://www.googleapis.com/auth/userinfo.email',
+	'https://www.googleapis.com/auth/userinfo.profile'
+];
+
 const router = express.Router();
 
-router.get('/google', passport.authenticate('google', { scope: ['email', 'profile'] }));
+router.get('/google', passport.authenticate('google', { scope: GOOGLE_SCOPES }));
 router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/auth' }), (req, res) => {
 	res.redirect('/');
 });
