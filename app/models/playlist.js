fce83400d1732a835a34b0ba5069338503a82df0
Zachary Yaro <zmyaro@zmyaro.com>
2024-05-23 18:00:49 -0400

    Change how playlist items are loaded to use fewer database queries

--- app/models/playlist.js
+++ app/models/playlist.js
@@ -3,7 +3,8 @@
 const mongoose = require('mongoose'),
 	Schema = mongoose.Schema,
 	PlaylistItem = require('./playlist_item.js'),
-	populateSong = require('../utils.js').populateSong;
+	Song = require('./song.js'),
+	{ SONG_AGGREGATE_POPULATE_STEPS, populateSong } = require('../utils.js');
 
 const playlistSchema = new Schema({
 	title: String,
@@ -20,42 +21,32 @@ const playlistSchema = new Schema({
  * @returns {Promise<Object>} Resolves with an object containing the playlist's document and a `songs` array of song documents
  */
 playlistSchema.statics.findByIdWithSongs = async function (id) {
-	const MAX_DEPTH = 1024; // TODO: Lower this when things start getting loaded in chunks.
-	
-	async function addItemToSongsArray(item, songsArr, depth) {
-		// Stop if the maximum playlist length has been exceeded.
-		if (depth >= MAX_DEPTH) {
-			return;
-		}
-		
-		// Add the item to the playlist.
-		await item.populate('song');
-		await populateSong(item.song);
-		var song = item.song.toObject();
-		song.itemId = item._id;
-		song.listIndex = songsArr.length;
-		songsArr.push(song);
-		
-		// Get the next item and recurse if there is one.
-		await item.populate('nextItem');
-		if (!item.$isEmpty('nextItem')) {
-			await addItemToSongsArray(item.nextItem, songsArr, depth + 1);
-		}
-	}
-	
-	// Get the playlist and make a basic object version to add the items to and return.
 	var playlist = await this.findById(id);
 	if (!playlist) { return; }
 	
-	var returnablePlaylist = Object.assign({ songs: [] }, playlist.toObject());
+	// Get the items in the playlist.
+	var playlistItems = await PlaylistItem.find({ playlist: playlist }),
+		playlistItemsMap = {};
+	playlistItems.forEach((playlistItem) => playlistItemsMap[playlistItem._id] = playlistItem);
 	
-	// Load the first item and then start recursively loading the rest of the list.
-	await playlist.populate('firstItem');
-	if (!playlist.$isEmpty('firstItem')) {
-		await addItemToSongsArray(playlist.firstItem, returnablePlaylist.songs, 0);
+	// Follow the linked list to build an ordered array of the playlist song IDs.
+	var nextPlaylistItem = playlistItemsMap[playlist.firstItem],
+		orderedSongIDs = [];
+	while (nextPlaylistItem) {
+		orderedSongIDs.push(nextPlaylistItem.song);
+		nextPlaylistItem = playlistItemsMap[nextPlaylistItem.nextItem];
 	}
 	
-	return returnablePlaylist;
+	var songs = await Song.aggregate([
+			{ $match: { _id: { $in: orderedSongIDs } } },
+			{ $addFields: {
+				listIndex: { $indexOfArray: [orderedSongIDs, '$_id'] }
+			} },
+			{ $sort: { listIndex: 1 } },
+			...SONG_AGGREGATE_POPULATE_STEPS
+		]),
+		playlistWithSongs = Object.assign({ songs: songs }, playlist.toObject());
+	return playlistWithSongs;
 };
 
 const Playlist = mongoose.model('Playlist', playlistSchema);
